52de186de4e7a7702306a2f7ef7e57b9a3960797
Schmidt Cristian Hernán <schcriher@gmail.com>
2019-05-19 02:03:49 -0300

    Fixes 5

--- bot/context.py
+++ bot/context.py
@@ -3,7 +3,7 @@
 import functools
 import threading
 
-from telegram import Update, ChatAction
+from telegram import Update
 from telegram.ext import Job
 
 from debug import flogger
@@ -68,7 +68,7 @@ class Context():
         del data[key]
 
 
-    @flogger
+    #@flogger
     def __get_last_data_key(self, keys, method):
         '''
         Search for the keys in the data and return
@@ -109,8 +109,8 @@ class Context():
         '''
         Sets the keys as the list of keys.
         '''
-        self.del_keys()
-        self.add_keys(*keys)
+        self.__keys.clear()
+        self.__keys.extend(keys)
 
 
     @flogger
@@ -177,7 +177,6 @@ class Context():
                 kwargs['update'] = update
                 chat = update.effective_chat
                 user = update.effective_user
-                bot.send_chat_action(chat.id, ChatAction.TYPING)
 
             elif isinstance(args[1], Job):
                 job = args[1]
@@ -208,7 +207,6 @@ class Context():
             with self.__lock:
                 self.set_keys(*keys)
                 result = func(**available_vars)
-                self.del_keys()
 
             return result
         return decorator
